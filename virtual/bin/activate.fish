2ae62ac24afad23aae7633c4fc94c97ff769324b
Gesare <gesareayienda@gmail.com>
2020-05-04 12:37:45 +0300

    Added app file auth

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/gesare/Documents/Python/one-min-pitch/virtual"
+set -gx VIRTUAL_ENV "/home/gesare/Documents/Python/Watchlist/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
